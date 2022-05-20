49975542aea205e114db3d0ac4ad23b267229355
lecepin <383810086@qq.com>
2022-05-20 20:38:38 +0800

    pretty code

--- electron/index.js
+++ electron/index.js
@@ -1,15 +1,15 @@
-import { app, BrowserWindow, Menu } from "electron";
-import log from "electron-log";
-import CONFIG from "./const";
-import { checkUpdate } from "./utils";
+import { app, BrowserWindow, Menu } from 'electron';
+import log from 'electron-log';
+import CONFIG from './const';
+import { checkUpdate } from './utils';
 
-app.commandLine.appendSwitch("--no-proxy-server");
+app.commandLine.appendSwitch('--no-proxy-server');
 
 function createWindow() {
   // Menu.setApplicationMenu(null);
   checkUpdate(
-    "https://cdn.jsdelivr.net/gh/lecepin/electron-react-tpl/package.json",
-    "https://github.com/lecepin/electron-react-tpl/releases"
+    'https://cdn.jsdelivr.net/gh/lecepin/electron-react-tpl/package.json',
+    'https://github.com/lecepin/electron-react-tpl/releases',
   );
 
   const mainWindow = new BrowserWindow({
@@ -25,21 +25,21 @@ function createWindow() {
   });
 
   mainWindow.loadURL(CONFIG.APP_START_URL);
-  CONFIG.IS_DEV && mainWindow.webContents.openDevTools();   
+  CONFIG.IS_DEV && mainWindow.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  app.on("activate", () => {
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
 });
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
